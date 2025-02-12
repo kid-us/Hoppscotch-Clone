cb1e065d86bb161c7cf98033204275659aa9716b
kid-us <kidushh29@gmail.com>
2025-02-12 18:26:55 +0300

    Using IconButton Component.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,48 +1,40 @@
-import Tippy from "@tippyjs/react";
-import { Globe, Link2, LoaderPinwheel, Settings } from "lucide-react";
+import { Globe, Hexagon, Link2, Settings } from "lucide-react";
+import IconButton from "../IconButton/IconButton";
 
 const Sidebar = () => {
   return (
     <>
-      <Tippy
-        content={<span className="text-[11px] font-semibold">Rest</span>}
-        placement="right"
-        theme="light"
+      <IconButton
+        name="Rest"
+        direction="right"
+        height="flex justify-center items-center border-l-[3px] border-btn h-12 w-full bg-search-bg"
       >
-        <button className="flex justify-center items-center border-l-[3px] border-btn h-[6.5vh] w-full bg-search-bg">
-          <Link2 size={17} />
-        </button>
-      </Tippy>
+        <Link2 size={17} />
+      </IconButton>
 
-      <Tippy
-        content={<span className="text-[11px] font-semibold">GraphQL</span>}
-        placement="right"
-        theme="light"
+      <IconButton
+        name="GraphQL"
+        direction="right"
+        height="flex justify-center items-center h-12 w-full hover:bg-search-bg"
       >
-        <button className="flex justify-center items-center h-[6.8vh] w-full text-gray-400 hover:text-white hover:bg-search-bg-hover">
-          <LoaderPinwheel size={17} />
-        </button>
-      </Tippy>
+        <Hexagon size={17} />
+      </IconButton>
 
-      <Tippy
-        content={<span className="text-[11px] font-semibold">Realtime</span>}
-        placement="right"
-        theme="light"
+      <IconButton
+        name="Realtime"
+        direction="right"
+        height="flex justify-center items-center h-12 w-full hover:bg-search-bg"
       >
-        <button className="flex justify-center items-center h-[6.8vh] w-full text-gray-400 hover:text-white hover:bg-search-bg-hover">
-          <Globe size={17} />
-        </button>
-      </Tippy>
+        <Globe size={17} />
+      </IconButton>
 
-      <Tippy
-        content={<span className="text-[11px] font-semibold">Settings</span>}
-        placement="right"
-        theme="light"
+      <IconButton
+        name="Settings"
+        direction="right"
+        height="flex justify-center items-center h-12 w-full hover:bg-search-bg"
       >
-        <button className="flex justify-center items-center h-[6.8vh] w-full text-gray-400 hover:text-white hover:bg-search-bg-hover">
-          <Settings size={17} />
-        </button>
-      </Tippy>
+        <Settings size={17} />
+      </IconButton>
     </>
   );
 };
